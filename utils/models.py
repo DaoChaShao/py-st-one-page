5efa239e535a9cd5b1d55bc37eb8335212670267
DaoChaShao <27882272@qq.com>
2025-08-21 22:40:21 +0800

    feat: add models.py with initial structure and metadata

---
+++ utils/models.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/8/21 22:36
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   models.py
+# @Desc     :   
+
+
