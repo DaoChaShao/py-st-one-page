25326c3e4fca400dcafc0d4ca68dfca98e1a6259
DaoChaShao <27882272@qq.com>
2025-08-22 13:29:30 +0800

    feat: add storyboard.py file with initial metadata

---
+++ subpages/storyboard.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/8/22 13:27
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   storyboard.py
+# @Desc     :
